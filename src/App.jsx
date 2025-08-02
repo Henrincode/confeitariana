c5f30f93b84c8b1192a255519badfa39fe1f24dc
Henrincode <spellctro@gmail.com>
2025-08-02 11:31:04 -0300

    feat: estruturando código para usar rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import Home from "./pages/Home"
+// import Sobre from "./pages/Sobre"
+// import Erro404 from "./pages/Erro404"
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="*" element={<Erro404 />} /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
