e8eedfff41e644ec1e20e02f4432c9cb27b26cc8
Henrincode <spellctro@gmail.com>
2025-08-03 15:12:04 -0300

    feat: criando páginas e scss para cada uma delas para testar as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import Home from "./pages/Home"
-// import Sobre from "./pages/Sobre"
-// import Erro404 from "./pages/Erro404"
+import Home from "./pages/Home/Home"
+import Sobre from "./pages/Sobre/Sobre"
+import Erro404 from "./pages/Erro404/Erro404"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
-        <Route path="*" element={<Erro404 />} /> */}
+        <Route path="*" element={<Erro404 />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import styles from './Home.module.scss'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Bem-vindo à Home</h1>
+            <p className={styles.description}>Essa é a página inicial do site.</p>
+        </div>
+    )
+}
