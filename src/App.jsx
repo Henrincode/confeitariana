083fb61bdf0263c05ae5eca8d977d426389a4234
Henrincode <spellctro@gmail.com>
2025-08-06 09:00:50 -0300

    feat: mudado jsx dos components para index.jsx e criado component Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home/Home"
-import Sobre from "./pages/Sobre/Sobre"
-import NotFound from "./pages/NotFound/NotFound"
-import Navbar from "./components/Navbar/Navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/";
+import Sobre from "./pages/Sobre/";
+import NotFound from "./pages/NotFound/";
+import Navbar from "./components/Navbar/";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="layout">
       <Navbar />
-      <main className="main">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/sobre" element={<Sobre />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </main>
-      <footer>
-        <strong>Footer</strong>
-      </footer>
+      <BrowserRouter className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,7 @@
+import style from './Footer.module.scss'
+
+export default function footer(){
+    return(
+        <div>Footer</div>
+    )
+}
