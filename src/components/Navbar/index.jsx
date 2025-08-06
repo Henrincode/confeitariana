8b5e0d9c7e7774fb571d3ea51ecdf50eb3168ad7
Henrincode <spellctro@gmail.com>
2025-08-06 20:02:29 -0300

    style: formatando estilo do component Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <header className={styles.header + " container"}>
-      <nav id="Navbar" className="box row">
+      <nav id="navbar" className={styles.navbar + " box row"}>
         <div className={styles.logo}>
-          Ana Marson<span>Confeiteira</span>
+          Ana Marson<span className={styles.textoSup}>Confeiteira</span>
         </div>
         <ul className="links">
           <li>
@@ -17,6 +17,24 @@ export default function Navbar() {
           </li>
           <li>
             <a href="#">Contato</a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/anamarsonconfeitaria"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i className="bi bi-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://wa.me/5519988521020?text=Olá, eu vim pelo site!"
+              target="_blank"
+              aria-label="Whatsapp"
+            >
+              <i className="bi bi-whatsapp"></i>
+            </a>
           </li>
         </ul>
       </nav>
