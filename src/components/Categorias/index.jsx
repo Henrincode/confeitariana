5034fb290d6fbe6d577eba0ae27e51c8dd3fea37
Henrincode <spellctro@gmail.com>
2025-08-12 20:13:26 -0300

    feat: criando component depoimentos

--- src/components/Categorias/index.jsx
+++ src/components/Categorias/index.jsx
@@ -1,4 +1,4 @@
-import categorias from "../../data/categorias.json";
+import DBcat from "../../data/categorias.json";
 import styles from "./Categorias.module.scss";
 import { charMax } from "../../utils";
 
@@ -6,7 +6,7 @@ export default function Categorias() {
   return (
     <>
       <div className={styles.categorias + " box row"}>
-        {categorias.map(({id, capa, nome }) => (
+        {DBcat.map(({ id, capa, nome }) => (
           <div key={id} className={styles.categoria}>
             <div className="imagem">
               <img src={`/img${capa}`} alt="" />
