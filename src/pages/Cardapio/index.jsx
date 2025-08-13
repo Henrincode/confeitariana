1cebfd9da3b2f53841e08c9209aa9c5a4065cb04
HENRIQUE ALAN RODRIGUES MARQUES <henrique.armarques@senacsp.edu.br>
2025-08-12 22:18:45 -0300

    feat: tornando função nomeCategoria() local na página cardápio

--- src/pages/Cardapio/index.jsx
+++ src/pages/Cardapio/index.jsx
@@ -1,16 +1,23 @@
-import { nomeCategoria } from "../../utils";
+// import { nomeCategoria } from "../../utils";
 import styles from "./Cardapio.module.scss";
 import DBprodut from "../../data/produtos.json";
 import DBcat from "../../data/categorias.json";
+
+
+// Pega a ID da categoria e retorna seu nome
+function nomeCategoria(id) {
+  return DBcat.find((categoria) => categoria.id === id).nome;
+}
 
 export default function Cardapio() {
   return (
     <div className={styles.body}>
       <div className={styles.titulo + " container"}>
         <div className={styles.lista + " box row"}>
+          {/* Criar divisão por categorias e diminuir o tamanho das imagens */}
           {DBprodut.map(({ id, categoria, img }) => {
             // Transforma ID da categoria em nome, relacionando com banco das categorias
-            const nomeCat = nomeCategoria(categoria, DBcat);
+            const nomeCat = nomeCategoria(categoria);
 
             return (
               <div key={id} className={styles.produto}>
