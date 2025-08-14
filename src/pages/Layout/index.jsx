a937435b9a4e7f934d190372a2c1cd4be18ba700
Henrincode <spellctro@gmail.com>
2025-08-14 12:45:44 -0300

    feat: scroll volta ao topo quando muda de página

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,9 +1,17 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, useLocation, Link } from "react-router-dom";
+import { useEffect } from "react";
 import styles from "./Layout.module.scss";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
 export default function Layout() {
+  const location = useLocation();
+
+  // Volta para o topo ao trocar de página
+  useEffect(() => {
+    window.scrollTo({ top: 0});
+  }, [location.pathname]);
+
   return (
     <>
       <div className="layout">
