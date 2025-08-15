f5304f2696e69df471212ab2ce4224e6e99555e6
Henrincode <spellctro@gmail.com>
2025-08-15 20:03:29 -0300

    feat: melhorando a página Catalogo

--- src/pages/Cardapio/index.jsx
+++ src/pages/Cardapio/index.jsx
@@ -1,4 +1,4 @@
-// import { nomeCategoria } from "../../utils";
+import { charMax } from "../../utils";
 import styles from "./Cardapio.module.scss";
 import DBprodut from "../../data/produtos.json";
 import DBcat from "../../data/categorias.json";
@@ -6,28 +6,42 @@ import DBcat from "../../data/categorias.json";
 // Pega a ID da categoria e retorna seu nome
 
 export default function Cardapio() {
-  const nomeCategoria = id => {
-    return DBcat.find(categoria => categoria.id === id).nome;
+  const nomeCategoria = (id) => {
+    return DBcat.find((categoria) => categoria.id === id).nome;
   };
 
   return (
     <div className={styles.body}>
       <div className={styles.titulo + " container"}>
-        <div className={styles.lista + " box row"}>
-          {/* Criar divisão por categorias e diminuir o tamanho das imagens */}
-          {DBprodut.map(({ id, categoria, img }) => {
-            // Transforma ID da categoria em nome, relacionando com banco das categorias
-            const nomeCat = nomeCategoria(categoria);
+        {/* Lista de categorias */}
+        {DBcat.map((categoria) => {
+          // Se categoria não tiver produto pula para a próxima categoria (loop)
+          if (!DBprodut.some((produto) => produto.categoria === categoria.id))
+            return;
 
-            return (
-              <div key={id} className={styles.produto}>
-                <img src={"img/cardapio/" + img} />
-                <div class={styles.idp}>#{id}</div>
-                <div className={styles.categoria}>{nomeCat}</div>
+          return (
+            <div>
+              {categoria.nome} <br />
+              {/* Lista de produtos */}
+              <div className={styles.lista + " box row"}>
+                {DBprodut.filter(
+                  (produto) => produto.categoria === categoria.id
+                ).map((produto) => {
+                  // Transforma ID da categoria em nome, relacionando com banco das categorias
+                  const nomeCat = charMax(nomeCategoria(produto.categoria), 15);
+
+                  return (
+                    <div key={produto.id} className={styles.produto}>
+                      <img src={"img/cardapio/" + produto.img} />
+                      <div class={styles.idp}>#{produto.id}</div>
+                      {/* <div className={styles.categoria}>{nomeCat}</div> */}
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
