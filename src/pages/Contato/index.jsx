912a4a5ed53d99122cb8c549b183536f064372c0
Henrincode <spellctro@gmail.com>
2025-08-18 06:28:54 -0300

    feat: adicionado simulador de envio de formulário

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -1,19 +1,35 @@
+import { useState } from "react";
 import styles from "./Contato.module.scss";
 import Divider from "../../components/Divider";
 
 export default function Contato() {
+  const [mensagem, setMensagem] = useState("");
+
+  function simulaEnvie(e) {
+    e.preventDefault();
+
+    // Limpa o form
+    e.target.reset();
+
+    // Mostra mensagem
+    setMensagem("✅ Formulário enviado com sucesso!");
+
+    // Esconde depois de 8s
+    setTimeout(() => setMensagem(""), 8000);
+  }
+
   return (
     <div className={styles.body}>
       <div className={styles.titulo + " container"}>
         <div className="box">
-
-
           {/* Mensagem */}
-
-          <section className={styles['msg-contato']}>
+          <section className={styles["msg-contato"]}>
             <h2>Alô, Alô! Vamos adoçar essa conversa?</h2>
-            <p>Se você deseja encomendar delícias artesanais ou tem alguma dúvida, fale comigo! Estou aqui para
-              atender você com carinho e açúcar na medida certa.</p>
+            <p>
+              Se você deseja encomendar delícias artesanais ou tem alguma dúvida,
+              fale comigo! Estou aqui para atender você com carinho e açúcar na
+              medida certa.
+            </p>
             <br />
             <p>Canais de atendimento:</p>
             <p><i className="bi bi-whatsapp"></i> 19 9 8852-1020</p>
@@ -25,28 +41,35 @@ export default function Contato() {
         </div>
 
         {/* Formulário de E-Mail */}
-
-        <form action="" className={styles.form + " box pad"}>
+        <form onSubmit={simulaEnvie} className={styles.form + " box pad"}>
           <div className={styles.linha + " box row"}>
             <div className={styles.campo + " box"}>
               <label htmlFor="f-nome">Seu nome*</label>
-              <input type="text" id="f-nome" placeholder="Escreva seu nome" required />
+              <input name="nome" type="text" id="f-nome" placeholder="Escreva seu nome" required />
             </div>
             <div className={styles.campo + " box"}>
               <label htmlFor="f-email">Seu E-Mail*</label>
-              <input type="text" id="f-email" placeholder="email@provedor.com" required />
+              <input name="mail" type="email" id="f-email" placeholder="email@provedor.com" required />
             </div>
             <div className={styles.campo + " box"}>
               <label htmlFor="f-cel">Seu celular / whatsapp*</label>
-              <input type="text" id="f-cel" placeholder="19 98888-4444" required />
+              <input name="cel" type="text" id="f-cel" placeholder="19 98888-4444" required />
             </div>
           </div>
           <div className={styles.campo}>
             <label htmlFor="f-msg">Mensagem*</label>
-            <textarea name="mensagem" id="f-msg" rows='6'
-              placeholder="Escreva uma mensagem" required></textarea>
+            <textarea
+              name="mensagem"
+              id="f-msg"
+              rows="6"
+              placeholder="Escreva uma mensagem"
+              required
+            ></textarea>
           </div>
-          <button>Enviar</button>
+          {/* Simula mensagem de email enviado */}
+          {mensagem && <div className="enviado">{mensagem}</div>}
+          <button type="submit">Enviar</button>
+
         </form>
       </div>
     </div>
