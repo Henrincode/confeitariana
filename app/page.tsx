a8bdbd57f9d27db90ddc82cb69534d0002c5fd76
Henrincode <spellctro@gmail.com>
2026-01-25 19:04:43 -0300

    feat: add login screen skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,50 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-dvh text-9xl text-pink-500">
-      Olá Mundo!
+    <div className="flex flex-col justify-center items-center h-dvh bg-amber-100">
+      <img className="w-100" src="/cookie-01.png" alt="" />
+      <h1 className="mb-2 font-bold text-6xl text-pink-400">Confeitariana</h1>
+      <form className="
+        flex
+        flex-col
+        gap-2
+        w-100
+        p-2
+        rounded-2xl
+        bg-pink-300
+
+        [&_.coluna]:flex
+        [&_.coluna]:flex-row
+        [&_.coluna]:justify-center
+        [&_.coluna]:gap-2
+
+        [&_input]:w-full
+        [&_input]:p-2
+        [&_input]:focus:outline-2
+        [&_input]:rounded-2xl
+        [&_input]:text-center
+        [&_input]:outline-pink-400
+        [&_input]:hover:outline-2
+        [&_input]:bg-white
+        [&_input]:hover:bg-pink-100
+
+        [&_button]:w-full
+        [&_button]:p-2
+        [&_button]:rounded-2xl
+        [&_button]:text-pink-800
+        [&_button]:bg-pink-100
+        [&_button]:hover:bg-pink-400
+        [&_button]:cursor-pointer
+      ">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="digite a senha" />
+        <div className="h-0.5 rounded-full bg-pink-200"></div>
+        <div className="coluna">
+          <button>Entrar</button>
+          <button>Cadastrar</button>
+        </div>
+        <p className="text-center text-sm text-pink-800 hover:text-pink-600 cursor-pointer">Esqueci minha senha</p>
+      </form>
     </div>
   );
 }
