6c54e6a34b003ac8ff1e916d60166f0a628b85d2
Henrincode <spellctro@gmail.com>
2026-02-11 04:04:53 -0300

    perf: disable client-side router cache while maintaining server-side caching

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // permite servidor baixar imagens externas
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'api.dicebear.com' },
       { protocol: 'https', hostname: '*.supabase.co' },
     ],
+  },
+  // invalida cache do usuário durante navegação
+  experimental: {
+    staleTimes: {
+      dynamic: 0, // Invalida o cache do navegador para rotas dinâmicas instantaneamente
+      static: 180, // Mantém o cache de rotas estáticas por 3 minutos (opcional)
+    },
   },
 };
 
