60f920b8859b00836d7a9f7576533824a6ed8781
Henrincode <henrincode@gmail.com>
2026-02-11 22:32:00 -0300

    feat: starting to create a modal to add addresses to the profile

--- app/admin/cliente/[id]/_components/DeleteProfile.tsx
+++ app/admin/cliente/[id]/_components/DeleteProfile.tsx
@@ -1,25 +1,25 @@
 import { deleteClient } from "@/server/actions/client.action";
 
 export default function UpdateProfile({ closeModal, client }: { closeModal: Function, client: any }) {
-    return(
-        <div onClick={(e) => e.stopPropagation()} className="flex flex-col items-center gap-4 max-w-100 p-2 mx-auto rounded-2xl border-4 border-white text-white bg-pink-400">
-            <div>Deseja realmente apagar o usuário "{client.name}"?</div>
+    return (
+        <div onClick={(e) => e.stopPropagation()} className="
+            flex flex-col items-center gap-4
+            max-w-100 p-2 mx-auto rounded-2xl border-4 
+            border-white text-white bg-pink-400
+        ">
+            <div>Deseja realmente apagar o usuário "<span className="text-pink-800">{client.name.split(' ')[0]}</span>"?</div>
             <div className="
                 flex flex-row gap-4
                 text-sm
                 cursor-pointer
                 select-none
 
-                [&_.buttom]:px-2
-                [&_.buttom]:py-1
-                [&_.buttom]:border-2
-                [&_.buttom]:rounded-xl
-                [&_.buttom]:text-white
-                [&_.buttom]:border-white
-                [&_.buttom]:bg-pink-500
-                [&_.buttom]:hover:bg-pink-800
+                [&_.buttom]:px-2 [&_.buttom]:py-1
+                [&_.buttom]:border-2 [&_.buttom]:rounded-xl
+                [&_.buttom]:text-white [&_.buttom]:border-white
+                [&_.buttom]:bg-pink-500 [&_.buttom]:hover:bg-pink-800
             ">
-                <div className="buttom" onClick={() => {closeModal(); deleteClient(client.id_client)}}>sim</div>
+                <div className="buttom" onClick={() => { closeModal(); deleteClient(client.id_client) }}>sim</div>
                 <div className="buttom" onClick={() => closeModal()}>não</div>
             </div>
         </div>
