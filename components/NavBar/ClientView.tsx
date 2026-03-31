7d167927b78ad5013e3dcd9fdf12f44794a4cc30
Henrincode <henrincode@gmail.com>
2026-03-30 22:15:16 -0300

    feat: creating mobile NavBar

--- components/NavBar/ClientView.tsx
+++ components/NavBar/ClientView.tsx
@@ -3,8 +3,10 @@
 import { logout } from "@/server/actions/auth";
 import { Session } from "next-auth";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
+import MobileNav from "./_components/MobileNav";
+import { usePathname } from "next/navigation";
 
 interface Props {
     className?: string
@@ -13,39 +15,74 @@ interface Props {
 
 export default function NavBarView({ className, session }: Props) {
 
+    const pathname = usePathname()
+
     const [modal, setModal] = useState(false)
 
-    document.body.classList.add('overflow-hidden')
+    function closeModal() {
+        setModal(false)
+    }
+
+    useEffect(() => {
+        if (modal) {
+            document.body.classList.add('overflow-hidden')
+        } else {
+            document.body.classList.remove('overflow-hidden')
+        }
+        return () => document.body.classList.remove('overflow-hidden')
+    }, [modal])
+
+    useEffect(() => {
+        closeModal()
+    }, [pathname])
 
     return (
         <>
             {/* Modal */}
             {modal && (
-                <div className="z-200 overflow-auto flex flex-col justify-center items-center fixed w-dvw h-dvh p-4 bg-gray-600/80">
-                    <div className="">
-                        <div className="w-200 h-20 bg-gray-400"></div>
-                        <div className="w-200 h-200 bg-gray-50"></div>
-                        <div className="w-200 h-20 bg-gray-800"></div>
+                <div onMouseDown={() => setModal(false)} className="z-200 flex flex-col justify-top items-center fixed w-full h-dvh bg-gray-600/80 md:backdrop-blur-xl">
+                    <div className="overflow-auto flex flex-col items-center w-full p-2">
+                        {/* component */}
+                        <div onMouseDown={(e) => e.stopPropagation()} className="w-full">
+                            <MobileNav pathname={pathname} closeModal={closeModal} />
+                        </div>
                     </div>
                 </div>
             )}
 
             <div className={`${className} z-100 p-2 border-b-2 border-amber-300 bg-amber-300/50 backdrop-blur`}>
                 <div className="box flex flex-row justify-between items-center gap-2">
-                    <Link href="/" id="nav-logo" className="flex-1 flex flex-row items-center gap-2 group">
+                    <Link href="/" id="nav-logo" className="flex flex-row items-center gap-2 group">
                         <img src="/cookie-01.png" alt="" className="w-8 brightness-95 saturate-140" />
                         <p className="flex-1 font-bold text-3xl text-center md:text-left text-pink-500 group-hover:text-pink-400 transition-all">Confeitariana</p>
                     </Link>
+
+                    <div className="flex-1"></div>
 
                     {/* mostra se estiver logad */}
                     {session && (
 
                         <>
                             {/* desktop */}
-                            <ul className="hidden md:flex flex-row gap-2 font text-amber-800 [&_.link]:hover:text-amber-500">
-                                <li><Link className="link" href="/admin/clientes">Clientes</Link></li>
-                                <li className="w-1 rounded-full bg-amber-400"></li>
-                                <li><Link className="link" href="/admin/clientes/cadastrar">Cadastrar cliente</Link></li>
+                            <ul className="
+                                hidden md:flex flex-row gap-2
+                                text-amber-800
+                                [&_.link]:text-white
+                                [&_.link]:hover:text-amber-800
+                                [&_.link]:whitespace-nowrap
+                            ">
+                                <li className="relative cursor-pointer group">
+                                    Clientes
+                                    <div className="
+                                    absolute left-1/2 -translate-x-1/2
+                                    hidden group-hover:block
+                                    ">
+                                        <div className="p-2 mt-1 rounded-lg bg-amber-500 shadow-md shadow-black/30">
+                                            <Link className="block link" href="/admin/clientes">Clientes</Link>
+                                            <Link className="block link" href="/admin/clientes/cadastrar">Cadastrar cliente</Link>
+                                        </div>
+                                    </div>
+                                </li>
                                 <li className="w-1 rounded-full bg-amber-400"></li>
                                 <li>
                                     <div className="flex flex-row gap-2 items-cente">
@@ -58,7 +95,7 @@ export default function NavBarView({ className, session }: Props) {
                             </ul>
 
                             {/* mobile */}
-                            <button className="md:hidden p-2 rounded text-amber-200 bg-amber-600"><FaBars /></button>
+                            <button onClick={() => setModal(true)} className="md:hidden p-2 rounded text-amber-200 bg-amber-600"><FaBars /></button>
                         </>
                     )}
                 </div>
--- components/NavBar/_components/MobileNav.tsx
+++ components/NavBar/_components/MobileNav.tsx
@@ -1,9 +1,72 @@
 'use client'
 
-export default function MobileNav() {
+import Link from "next/link"
+
+interface Params {
+    pathname: string
+    closeModal: () => void
+}
+
+export default function MobileNav({ pathname, closeModal }: Params) {
     return (
-        <div>
-            teste
+        <div onMouseDown={(e) => e.stopPropagation()} className="
+            flex flex-col gap-8
+            w-full lg:w-auto p-8 rounded-2xl text-white bg-gray-800
+            [&_p]:text-4xl
+            
+            [&_.area]:flex
+            [&_.area]:flex-row
+            [&_.area]:gap-2
+            
+            [&_.area-border]:w-1.5
+            [&_.area-border]:rounded-full
+            [&_.area-border]:bg-pink-500
+            
+            [&_ul]:flex
+            [&_ul]:flex-col
+            [&_ul]:gap-1
+
+            [&_li]:text-2xl
+            [&_li]:ml-4
+            [&_li]:hover:text-pink-500
+            
+            [&_.divider]:w-full
+            [&_.divider]:h-1
+            [&_.divider]:rounded-full
+            [&_.divider]:bg-gray-700
+
+            [&_.sub]:mt-2
+            [&_.sub]:ml-4
+            [&_.sub]:text-sm
+            [&_.sub]:font-semibold
+            [&_.sub]:cursor-auto
+            [&_.sub]:text-gray-500
+        ">
+            <div className="area">
+                <div className="area-border"></div>
+                <ul>
+                    <p>Clientes</p>
+                    <li>
+                        <Link
+                            onClick={() => pathname === '/admin/clientes' && closeModal()}
+                            href={'/admin/clientes'}>
+                            Ver todos
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={() => pathname === '/admin/clientes/cadastrar' && closeModal()}
+                            href={'/admin/clientes/cadastrar'}>
+                            Cadastrar novo
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            {/* divider */}
+            {/* <div className="divider"></div> */}
+
+            
         </div>
     )
 }
