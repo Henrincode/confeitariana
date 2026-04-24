d39749b11220dd1f4ebfc745d9dda7dad75272aa
Henrincode <spellctro@gmail.com>
2026-04-24 12:22:02 -0300

    feat: working in categories modal

--- components/modals/products/categories.tsx
+++ components/modals/products/categories.tsx
@@ -2,7 +2,9 @@
 
 import { FaBoxOpen } from "react-icons/fa"
 import Modals from ".."
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { ProductCategory } from "@/types/product.types"
+import { findProductCategories } from "@/server/actions/products.action"
 
 interface Props {
     closeModal: () => void
@@ -10,7 +12,21 @@ interface Props {
 
 export default function ModalProductCategories({ closeModal }: Props) {
 
+    useEffect(() => {
+        load()
+    }, [])
+
+    const [categoriesDB, setCategoriesDB] = useState<ProductCategory[]>()
     const [btnSelected, setBtnSelected] = useState('create')
+
+    async function load() {
+        try {
+            const response = await findProductCategories()
+            setCategoriesDB(response.data)
+        } catch (error) {
+            console.log('Erro interno do servidor')
+        }
+    }
 
     return (
         <Modals closeModal={closeModal}>
@@ -50,19 +66,69 @@ export default function ModalProductCategories({ closeModal }: Props) {
                         <button
                             onClick={() => setBtnSelected('edit')}
                             className={btnSelected === 'edit' ? 'selected' : ''}
-                        >Editar</button>
+                        >
+                            Editar
+                        </button>
                         <button
                             onClick={() => setBtnSelected('delete')}
                             className={btnSelected === 'delete' ? 'selected' : ''}
-                        >Apagar</button>
+                        >
+                            Apagar
+                        </button>
                     </div>
+
+                    {/* line */}
+                    <div className="h-0.5 my-2 -mx-2 bg-gray-300"></div>
                     {/* formulários */}
 
                     {btnSelected === 'create' && (
-                        <form action="#">
-                            <p>Criar categorias de produtos</p>
-                            <p>Escreva o nome da categoria no campo abaixo e clique em criar</p>
-                        </form>
+                        <>
+                            <p className="font-semibold text-xl text-gray-600">
+                                Criar categorias de produtos
+                            </p>
+                            <p className="text-gray-500">
+                                Escreva o nome da categoria no campo abaixo e clique em criar.
+                            </p>
+                            <form action="#" className="
+                                grid grid-cols-5 items-end gap-4
+                                mt-4
+
+                                [&_.label]:pl-2
+                                [&_.label]:text-sm
+                                [&_.label]:font-semibold
+                                [&_.label]:text-gray-500
+
+                                [&_.input]:outline-pink-400
+                                [&_.input]:bg-gray-200
+                                [&_.input]:border-2
+                                [&_.input]:border-gray-400
+                                [&_.input]:p-1
+                                [&_.input]:rounded-lg
+                                [&_.input]:text-gray-800
+                            ">
+
+                                <div className="flex flex-col col-span-2">
+
+                                    <label htmlFor="categories" className="label">Categoria pai</label>
+
+                                    <select name="categories" id="categories" className="input">
+                                        <option value="0">--</option>
+                                        {categoriesDB?.map(c => (
+                                            <option key={c.id_product_category} value={c.id_product_category}>{c.name}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className="flex flex-col col-span-2">
+                                    <label htmlFor="category" className="label">Nova categoria</label>
+                                    <input name="category" id="category" type="text" placeholder="Nome da categoria" className="input" />
+                                </div>
+                                <div className="flex flex-col col-span-1">
+                                    <button className="w-full p-1 border-2 rounded-lg text-pink-600 border-pink-400 bg-pink-200 hover:bg-pink-300 cursor-pointer">
+                                        Criar
+                                    </button>
+                                </div>
+                            </form>
+                        </>
                     )}
 
                     {btnSelected === 'edit' && (
@@ -83,6 +149,6 @@ export default function ModalProductCategories({ closeModal }: Props) {
                     */}
                 </div>
             </div>
-        </Modals>
+        </Modals >
     )
 }
